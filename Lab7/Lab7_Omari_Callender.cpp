5bdab8c0e0366615fe069976df841173704c335c
Omarc33 <omaricallender00@gmail.com>
2026-06-03 18:01:48 +0000

    completed lab 7

---
+++ Lab7/Lab7_Omari_Callender.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n---- example 1: bool variable"<<endl;
+    //bool variable to check if a number is positive
+    //declare variables
+
+    int n;
+    bool checkpositive; // bool values are 0(false) or 1 (truth)
+
+    //collect the number
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    //check if a number is positive
+    checkpositive = n > 0;
+
+    //print result
+
+    cout<<" Is number "<<n<<" positive ? "<<checkpositive<<endl;
+
+    //update checkpositive
+    checkpositive = '%';
+
+    //for a bool value that is not 0 or 1, the retrurn is always 1
+    cout<<"updated checkpositive = "<<checkpositive<<endl;
+
+
+    cout<<"\n---- example 2: if statement----"<<endl;
+
+    //check if n is zero
+    int c= 10;
+    if (n==0){
+        cout<<"n is zero!";
+        c+=20;
+
+    }
+
+    cout<<"\t updated c="<<c<<endl;
+
+    cout <<"\n----example 3: if - else statement"<<endl;
+    //if else behaves as an on/off switch
+    //check if a number is even or odd,Even numbers are divisible by 2, meaning that when the number is divided by 2, the remainder is 0.
+
+    if (n%2==0){
+        cout<<n<<" is an EVEN number"<<endl;
+    }
+
+    else{
+        cout<<n<<" is an ODD number"<<endl;
+    }
+
+    cout<<"\n----example 4: if else statement "<<endl;
+    /*
+    - ultraviolet: less than 379 nm and greater than or equal to 10 nm
+    -Blue: 380 nm to 520 nm
+    -Green: 521 nm to 590 nm
+    -Red: 591 nm to 740 nm
+    -infrared: more than 741 nm
+    -Any wavelength less than 10 nm is undefined 
+    */
+   // declare variables
+    int wavelength = 0;
+    string emittedlight = "";
+
+    //updated wavelength
+    cout<<"Enter the wavelength: ";
+    cin>>wavelength;
+    
+    //mulitway
+
+    if (wavelength < 379 && wavelength >=10) {emittedlight = "ULTRAVIOLET";}
+    else if (wavelength >=520 && wavelength <=380) {emittedlight = "BLUE";}
+    else if (wavelength >=590 && wavelength <=521) {emittedlight = "GREEN";}
+    else if (wavelength >=740 && wavelength <=591) {emittedlight = "RED";}
+    else if (wavelength >741) {emittedlight = "INFRARED";}
+    else {emittedlight = "UNDEFINED";}
+
+    //print result
+    cout<<wavelength<<"nm emits "<<emittedlight<<" light"<<endl;
+
+    cout<<"\n---example 5: conditional operator---"<<endl;
+    // conditional operator: three operands--> condition ? expression1 (true) , expression2(false)
+    /*
+    example 3)
+    if(n%2==0){
+        cout<<n<<" is an EVEN number"<<endl;
+    }
+
+    else{
+        cout<<n<<" is an ODD number"<<endl;
+    }
+    */
+
+    cout<<n<<(n%2==0 ? " is an EVEN number" : " is an ODD number")<<endl;
+
+    cout<<"\n---example 6: conditional operator---"<<endl;
+    //check if n is greater than 10. if n is greater than 10, set the max value to n.
+    int max = 10;
+    bool checkmax=false;
+    //if else
+    if (n>max){checkmax = true;}
+    else{checkmax = false;}
+    // conditioanl operands to check if n is greater than max.
+    checkmax = n>max ? true : false;
+
+    //print result
+    cout<<"Is n greater than 10: "<<checkmax<<endl;
+
+    cout<<"\n---example 7: nested conditional statements---"<<endl;
+    //check if a number ,n, is positive (even or odd),negative (even or odd) ,or zero
+    //multiway statement
+    /*
+    if(n>0 && n%2==0){
+        cout<<"positive and even number "<<endl;
+
+
+    }
+    else if (n>0 && n%2!=0){
+        cout<<"positive and odd number "<<endl;
+    }
+    else if (n<0 && n%2==0){
+        cout<<"negative and even number "<<endl;
+    }
+    else if (n<0 && n%2!=0){
+        cout<<"negative and odd number "<<endl;
+    }
+    else{
+        cout<<"zero"<<endl;
+    }
+     */
+    if(n>0){
+        //inner conditional statement
+        if(n%2==0){
+            cout<<"positive and even number "<<endl;
+        }
+        else{
+            cout<<"positive and odd number "<<endl;
+        }
+    }
+    else if (n<0){
+        
+        if (n%2==0){
+            cout<<"negative and even number "<<endl;
+        }
+        else{
+            cout<<"negative and odd number "<<endl;
+        }
+    }
+    else{
+        cout<<"zero"<<endl;
+    }
+    return 0;
+
+
+}
